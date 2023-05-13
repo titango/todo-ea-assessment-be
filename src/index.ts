f91c20bff31223f44b9691c06e33a88b5e4d1427
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2023-05-12 21:32:27 -0700

    reconfig index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import http from "http";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app: Express = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
 
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT;
+const server = http.createServer(app);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server!!");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
