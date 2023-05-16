1f6c3adc3f2431c62b056caa96c3a22d830d5e56
Tan Thinh Ngo <ngotanthinh92@gmail.com>
2023-05-16 12:57:32 -0700

    Make task updated using socket.io

---
+++ src/api/v1/services/socket.service.ts
@@ -0,0 +1,35 @@
+import http from "http";
+import { Server } from "socket.io";
+
+import Singleton from "../../../helpers/singleton";
+import TaskService from "./task.service";
+
+class SocketService extends Singleton {
+  private io: Server | null = null;
+
+  public connectServer(server: http.Server) {
+    this.io = new Server(server, {
+      cors: {
+        origin: `${process.env.CLIENT_SCHEME}://${process.env.CLIENT_HOST}:${process.env.CLIENT_PORT}`,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+      },
+    });
+    this.io.on("connection", (socket) => {
+      console.log(`User connected: ${socket.id}`);
+    });
+  }
+
+  public async execTaskUpdated() {
+    if (!this.io) return;
+    try {
+      const taskService = new TaskService();
+      const allTasks = await taskService.getTasks();
+      console.log("emitting");
+      this.io.emit("taskUpdated", allTasks);
+    } catch (err) {
+      // Error emitting
+    }
+  }
+}
+
+export default SocketService;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import http from "http";
+import { Server } from "socket.io";
 
 import app from "./app";
 import Database from "./api/v1/database";
+import SocketService from "./api/v1/services/socket.service";
 
 const scheme = process.env.SCHEME;
 const host = process.env.HOST;
 const port = process.env.PORT;
 const server = http.createServer(app);
+
+const socketService = SocketService.getInstance<SocketService>();
+socketService.connectServer(server);
+
+setInterval(() => {
+  socketService.execTaskUpdated();
+}, 10 * 1000);
 
 server.listen(port, async () => {
   console.log(`⚡️[server]: Server is running at ${scheme}://${host}:${port}`);
